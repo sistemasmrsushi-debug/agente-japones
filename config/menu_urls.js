8f73fc3dc0399771a9ebe40f4137b4c111cc7266
Diego <sistemasmrsushi@gmail.com>
2026-06-23 20:12:01 -0600

    fix: URLs completas con slug en menu_urls.js

--- config/menu_urls.js
+++ config/menu_urls.js
@@ -1,63 +1,60 @@
 // config/menu_urls.js
-// URLs del menu en mrsushi.mx/pedir
-// Se usa para compartir link cuando el cliente pregunta por un platillo
-
 const BASE = "https://www.mrsushi.mx/pedir";
 
 module.exports = {
   // Sushi 2x1
-  "Sushi Tunagui":              `${BASE}/9nRR2HtX9kAArA9Dr`,
-  "Sushi Tuna King":            `${BASE}/MHZ8uDALZ6Db39m4j`,
-  "Sushi Tempura Maki":         `${BASE}/sY483mBreM4KkpgMs`,
-  "Sushi Mr. Kakiage":          `${BASE}/KHPpCmkPBkHLWs9YE`,
-  "Sushi Kani Maki":            `${BASE}/aATRQATNYwqZpoHRx`,
-  "Sushi Mr. Tampico Habanero": `${BASE}/DtPTskB5862xEWwPC`,
-  "Sushi Avocado Teriyaki":     `${BASE}/EBXgFE9dpTEDxZrgp`,
-  "Sushi Spicy Tuna":           `${BASE}/rzhtarXQueGoJuMbr`,
-  "Sushi Mr. Tempura":          `${BASE}/HkP83vp37bnWZSzkR`,
-  "Sushi Blue Cheese":          `${BASE}/9erPvpStS9xBpvLMy`,
-  "Sushi Hot Mango":            `${BASE}/G9eeuQTczkaPQ5LXG`,
+  "Sushi Tunagui":              `${BASE}/9nRR2HtX9kAArA9Dr/2x1-sushi-tunagui-atun-anguila-masago-y-aguacate`,
+  "Sushi Tuna King":            `${BASE}/MHZ8uDALZ6Db39m4j/2x1-sushi-tuna-king-atun-picante-aguacate-y-chile-tempura`,
+  "Sushi Tempura Maki":         `${BASE}/sY483mBreM4KkpgMs/2x1-sushi-tempura-maki-queso-crema-tempura-cangrejo-y-chile`,
+  "Sushi Mr. Kakiage":          `${BASE}/KHPpCmkPBkHLWs9YE/2x1-sushi-mr-kakiage-verduras-tempura-cangrejo-y-queso`,
+  "Sushi Kani Maki":            `${BASE}/aATRQATNYwqZpoHRx/2x1-sushi-kani-maki-cangrejo-queso-crema-aguacate-y-pepino`,
+  "Sushi Mr. Tampico Habanero": `${BASE}/DtPTskB5862xEWwPC/2x1-sushi-mr-tampico-habanero-tampico-picante-cangrejo-y-queso`,
+  "Sushi Avocado Teriyaki":     `${BASE}/EBXgFE9dpTEDxZrgp/2x1-sushi-avocado-teriyaki-pollo-crunchy-aguacate-y-zanahoria`,
+  "Sushi Spicy Tuna":           `${BASE}/rzhtarXQueGoJuMbr/2x1-sushi-spicy-tuna-atun-picante-con-teriyaki`,
+  "Sushi Mr. Tempura":          `${BASE}/HkP83vp37bnWZSzkR/2x1-sushi-mr-tempura-manchego-capeado-cangrejo-y-chipotle`,
+  "Sushi Blue Cheese":          `${BASE}/9erPvpStS9xBpvLMy/2x1-sushi-blue-cheese-arrachera-queso-azul-y-costra-crujiente`,
+  "Sushi Hot Mango":            `${BASE}/G9eeuQTczkaPQ5LXG/2x1-sushi-hot-mango-kanikama-mango-y-chile-serrano`,
 
   // Combos
-  "Combo Mr. 4x4":              `${BASE}/AqreuQsp5Dhxnmgjb`,
-  "Combo Lunch Box":            `${BASE}/bcr89fvv9vh9WkEAG`,
+  "Combo Mr. 4x4":              `${BASE}/AqreuQsp5Dhxnmgjb/combo-mr-4x4-4-medios-rollos-a-elegir-entre-18-opciones`,
+  "Combo Lunch Box":            `${BASE}/bcr89fvv9vh9WkEAG/combo-lunch-box-entrada-arroz-rollo-y-agua`,
 
   // Sushi Box
-  "Sushi Box Clasico":          `${BASE}/AqNejsm3cAYYbGP4P`,
-  "Sushi Box Light":            `${BASE}/JAFxNfgFptadQxF24`,
-  "Entrada Box":                `${BASE}/nA6MCF5YfRdnTQqbt`,
+  "Sushi Box Clasico":          `${BASE}/AqNejsm3cAYYbGP4P/sushi-box-clasico-6-rollos-seleccionados-en-charola-para-compartir`,
+  "Sushi Box Light":            `${BASE}/JAFxNfgFptadQxF24/sushi-box-light-4-rollos-y-2-entradas-seleccionadas`,
+  "Entrada Box":                `${BASE}/nA6MCF5YfRdnTQqbt/entrada-box-5-entradas-seleccionadas-en-charola`,
 
   // Entradas
-  "Edamames Asados":            `${BASE}/9uZ4WpRqzDBFJdD6S`,
-  "Edamames Spicy":             `${BASE}/NMb6JB77z8bxmSscn`,
-  "Chiles Tempura":             `${BASE}/8vCptJSARdBWsKS8A`,
-  "Dumplings de Vegetales":     `${BASE}/QgebTp75RoJZh6GA7`,
-  "Dumplings de Vegetales Fritos": `${BASE}/QT8kfzqFtGZqsf2NJ`,
-  "Dumplings de Cerdo":         `${BASE}/rTQPapaFkjNJhrvP8`,
-  "Dumplings de Cerdo Fritos":  `${BASE}/Wv3yL5qpdxKjBQMWd`,
+  "Edamames Asados":            `${BASE}/9uZ4WpRqzDBFJdD6S/edamames-asados-vainas-de-soya-asadas-con-sal-de-mar`,
+  "Edamames Spicy":             `${BASE}/NMb6JB77z8bxmSscn/edamames-spicy-vainas-de-soya-asadas-con-salsa-spicy`,
+  "Chiles Tempura":             `${BASE}/8vCptJSARdBWsKS8A/chiles-tempura-jalapenos-rellenos-de-queso-y-kanikama`,
+  "Dumplings de Vegetales":     `${BASE}/QgebTp75RoJZh6GA7/dumplings-de-vegetales-ravioles-al-vapor-rellenos-de-vegetales`,
+  "Dumplings de Vegetales Fritos": `${BASE}/QT8kfzqFtGZqsf2NJ/dumplings-de-vegetales-fritos-ravioles-crujientes-de-vegetales`,
+  "Dumplings de Cerdo":         `${BASE}/rTQPapaFkjNJhrvP8/dumplings-de-cerdo-ravioles-al-vapor-rellenos-de-lomo`,
+  "Dumplings de Cerdo Fritos":  `${BASE}/Wv3yL5qpdxKjBQMWd/dumplings-de-cerdo-fritos-ravioles-crujientes-de-lomo`,
 
   // Sopas
-  "Sopa Miso":                  `${BASE}/DK2dPnkDYKNmNEJYm`,
-  "Sopa T-Udon":                `${BASE}/n4577suHswtwSK5Sx`,
-  "Sopa Coco Ramen":            `${BASE}/QmFxCB8EnSZN8puox`,
-  "Sopa Ramen Bacon":           `${BASE}/hCkHBsdCJMkCHCfYe`,
+  "Sopa Miso":                  `${BASE}/DK2dPnkDYKNmNEJYm/sopa-miso-tofu-y-cebollin-en-caldo-de-miso-y-soya`,
+  "Sopa T-Udon":                `${BASE}/n4577suHswtwSK5Sx/sopa-t-udon-camaron-empanizado-y-verduras-en-caldo-udon`,
+  "Sopa Coco Ramen":            `${BASE}/QmFxCB8EnSZN8puox/sopa-coco-ramen-pollo-huevo-y-verduras-en-caldo-de-coco`,
+  "Sopa Ramen Bacon":           `${BASE}/hCkHBsdCJMkCHCfYe/sopa-ramen-cerdo-tocineta-ahumada-huevo-y-espinaca-en-caldo-japones`,
 
   // Brochetas Kushiagues
-  "Kushiague de Queso":         `${BASE}/vt7TaBvhGacFvPr6h`,
-  "Kushiague de Platano":       `${BASE}/38PjDQyLm6pdev4hc`,
-  "Kushiague de Camaron":       `${BASE}/e45nX4Z5q3mreCwEN`,
+  "Kushiague de Queso":         `${BASE}/vt7TaBvhGacFvPr6h/kushiague-de-queso-brochetas-empanizadas-rellenas-de-queso-fundido-3-pzas`,
+  "Kushiague de Platano":       `${BASE}/38PjDQyLm6pdev4hc/kushiague-de-platano-brochetas-de-queso-y-platano-empanizado-3-pzas`,
+  "Kushiague de Camaron":       `${BASE}/e45nX4Z5q3mreCwEN/kushi-ebi-queso-camaron-empanizado-con-queso-fundido-3-pzas`,
 
   // Arroz
-  "Arroz Gohan":                `${BASE}/HcWGWq8AEybxqsy6u`,
-  "Yakimeshi Verduras":         `${BASE}/FybF4mqdGvWucPBLK`,
+  "Arroz Gohan":                `${BASE}/HcWGWq8AEybxqsy6u/gohan-arroz-japones-al-vapor`,
+  "Yakimeshi Verduras":         `${BASE}/FybF4mqdGvWucPBLK/yakimeshi-arroz-salteado`,
 
   // Rollos Tradicionales
-  "Sushi Tuna Maki":            `${BASE}/JCaWAEvToAsXmDG9f`,
-  "Sushi Ebi Maki":             `${BASE}/ZprTpmhrYbtK68Axj`,
-  "Sushi Unagui Maki":          `${BASE}/Ktk33eZPX23oTassh`,
-  "Sushi Avocado Maki Atun":    `${BASE}/2nF7b7SPb2Z96Pj5v`,
-  "Sushi California Maki Atun": `${BASE}/2nF7b7SPb2Z96Pj5v`,
+  "Sushi Tuna Maki":            `${BASE}/JCaWAEvToAsXmDG9f/sushi-tuna-maki-atun-clasico-con-queso`,
+  "Sushi Ebi Maki":             `${BASE}/ZprTpmhrYbtK68Axj/sushi-ebi-maki-camaron-y-queso-tradicional`,
+  "Sushi Unagui Maki":          `${BASE}/Ktk33eZPX23oTassh/sushi-unagui-maki-anguila-glaseada-clasica`,
+  "Sushi Avocado Maki":         `${BASE}/2nF7b7SPb2Z96Pj5v/sushi-avocado-maki-con-queso-aguacate-y-proteina-a-elegir`,
+  "Sushi California Maki":      `${BASE}/2nF7b7SPb2Z96Pj5v/sushi-avocado-maki-con-queso-aguacate-y-proteina-a-elegir`,
 
   // Menu completo
-  "_menu_completo": `${BASE}`,
+  "_menu_completo": BASE,
 };
